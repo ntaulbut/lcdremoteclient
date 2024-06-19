9a27aff4be5a93f23d7890ff5672f0f7b5b20327
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-06-19 20:36:35 +0100

    update

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from tkinter import ttk
 import requests
 
 HTTP_NO_CONTENT = 204
+HTTP_OK = 200
 MESSAGE_RESOURCE_URL = "http://192.168.1.207:8080/api/message/"
 
 
-def set_message():
+def set_message() -> None:
     message = {
         "row_one": row_one_string.get(),
         "row_two": row_two_string.get()
@@ -24,12 +25,13 @@ def set_message():
         return
 
     if r.status_code == HTTP_NO_CONTENT:
-        messagebox.showinfo("Success", message="Successfully set.")
+        row_one_remote_string.set(row_one_string.get())
+        row_two_remote_string.set(row_two_string.get())
     else:
         messagebox.showerror("Error", message="Command failed.")
 
 
-def clear_message():
+def clear_message() -> None:
     try:
         r = requests.delete(MESSAGE_RESOURCE_URL)
     except requests.exceptions.ConnectionError:
@@ -40,9 +42,30 @@ def clear_message():
         return
 
     if r.status_code == HTTP_NO_CONTENT:
-        row_one_string.set("")
-        row_two_string.set("")
-        messagebox.showinfo("Success", message="Successfully cleared.")
+        row_one_remote_string.set("")
+        row_two_remote_string.set("")
+    else:
+        messagebox.showerror("Error", message="Command failed.")
+
+
+def get_message() -> None:
+    try:
+        r = requests.get(MESSAGE_RESOURCE_URL)
+    except requests.exceptions.ConnectionError:
+        if messagebox.showerror("Error",
+                                message="Couldn't connect to the device.",
+                                type="retrycancel") == "retry":
+            set_message()
+        return
+
+    if r.status_code == HTTP_OK:
+        message = r.text.split("\n")
+        if len(message) == 2:
+            row_one_remote_string.set(message[0])
+            row_two_remote_string.set(message[1])
+        else:
+            row_one_remote_string.set("")
+            row_two_remote_string.set("")
     else:
         messagebox.showerror("Error", message="Command failed.")
 
@@ -58,14 +81,25 @@ menu_frame.grid(padx=20, pady=20)
 row_one_string = tk.StringVar()
 row_two_string = tk.StringVar()
 
-row_one_entry = ttk.Entry(menu_frame, textvariable=row_one_string)
-row_one_entry.grid(column=0, row=0, columnspan=2, pady=5, sticky="ew")
-row_two_entry = ttk.Entry(menu_frame, textvariable=row_two_string, width=27)
-row_two_entry.grid(column=0, row=1, columnspan=2, sticky="ew")
+row_one_remote_string = tk.StringVar()
+row_two_remote_string = tk.StringVar()
 
-clear_button = ttk.Button(menu_frame, text="Clear", command=clear_message)
+row_one_entry = ttk.Entry(menu_frame, textvariable=row_one_string, width=16)
+row_one_entry.grid(column=0, row=0, pady=0)
+row_two_entry = ttk.Entry(menu_frame, textvariable=row_two_string, width=16)
+row_two_entry.grid(column=0, row=1, pady=0)
+
+row_one_remote_label = ttk.Label(menu_frame, textvariable=row_one_remote_string, width=16, borderwidth=2, relief="sunken")
+row_one_remote_label.grid(column=2, row=0, sticky="ew")
+row_two_remote_label = ttk.Label(menu_frame, textvariable=row_two_remote_string, width=16, borderwidth=2, relief="sunken")
+row_two_remote_label.grid(column=2, row=1, sticky="ew")
+
 set_button = ttk.Button(menu_frame, text="Set", command=set_message)
-clear_button.grid(column=0, row=2, pady=10, sticky="ew")
-set_button.grid(column=1, row=2, sticky="ew")
+clear_button = ttk.Button(menu_frame, text="Clear", command=clear_message)
+get_button = ttk.Button(menu_frame, text="Get", command=get_message)
+set_button.grid(column=0, row=2, sticky="ew")
+clear_button.grid(column=0, row=3, sticky="ew")
+get_button.grid(column=2, row=2, sticky="ew")
 
+get_message()
 window.mainloop()
